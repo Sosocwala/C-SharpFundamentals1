f63ab1d453bc84f6f70a2221df1ba49bdd2ae4a9
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-16 13:19:59 +0200

    C# fundamentals

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -16,8 +16,11 @@ namespace Exercise1
 
         static void Main(string[] args)
         {
-            
-            
+            Apple cost = new Apple();
+            double total = cost.Calc(12, 5);
+            Console.WriteLine("The amount of 12 Apples : {0:C}", total);
+            Console.WriteLine("Press Enter to Exit");
+
         }
 
     }
