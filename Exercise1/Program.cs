646563e1daf2061ba7d7b4273da45df936bd1b55
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-16 12:33:34 +0200

    C# fundamentals

---
+++ Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise1
+{
+    class Program
+    {
+        public class Apple
+        {
+          
+             
+        }
+
+        static void Main(string[] args)
+        {
+          
+            
+        }
+
+    }
+}
