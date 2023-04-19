b42b1d26f64fc0967deebb927a28ba72de13a152
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-19 10:35:00 +0200

    C# fundamentals

--- introToForAndForeachLoops/Program.cs
+++ introToForAndForeachLoops/Program.cs
@@ -6,7 +6,10 @@ namespace introToForAndForeachLoops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
