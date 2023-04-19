16eb8085cb879186a2ca5334fb71e78bb9788c95
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-19 10:14:27 +0200

    C# fundamentals

---
+++ introToWhileAndDoWhileLoops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace introToWhileAndDoWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
