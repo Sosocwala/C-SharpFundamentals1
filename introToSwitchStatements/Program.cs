9a002ead10edcd83f02043e15359958034c2abc2
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-19 09:07:07 +0200

    C# fundamentals

---
+++ introToSwitchStatements/Program.cs
@@ -0,0 +1,22 @@
+﻿b  using System;
+
+namespace introToSwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checkCoulor("pink");
+        }
+        public static string checkCoulor(string choice)
+        {
+            string peakCoulor;
+            switch (choice)
+            {
+                case "red":
+                    peakCoulor = "";
+
+            }    
+        }
+    }
+}
