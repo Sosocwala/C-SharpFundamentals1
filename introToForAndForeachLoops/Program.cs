f45c5e5a0fa4bf305e55a95ba74b6bacd1dfaa67
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-19 10:43:33 +0200

    C# fundamentals

--- introToForAndForeachLoops/Program.cs
+++ introToForAndForeachLoops/Program.cs
@@ -6,7 +6,15 @@ namespace introToForAndForeachLoops
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 100; i++)
+            //for loop
+            for (int i = 0; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            //foreach loop
+            int[] counter = { 10, 20, 30, 40, 50, 55, 60, 65, 70, 75, 80, 85, 90, 100 };
+            foreach (int i in counter)
             {
                 Console.WriteLine(i);
             }
