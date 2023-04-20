5af3eccc20dc3b04d7a4fec04903c80239eee308
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-20 18:52:05 +0200

    C# fundamentals

---
+++ randomListOfNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace randomListOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] random = new int[100];
+            byte k = 0;
+            Random randomNums = new Random();
+            bool addToArray = true;
+
+            while (k <= 100)
+            {
+
+            }
+        }
+    }
+}
