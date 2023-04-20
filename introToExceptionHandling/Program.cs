9bf92cde3b1eeb422ff7b46b1e6886180b4e11d7
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-20 16:27:38 +0200

    C# fundamentals

---
+++ introToExceptionHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace introToExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 12;
+            try
+            {
+                num = num / int.Parse("0");
+                Console.WriteLine("{0}", num);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+        }
+    }
+}
--- introToRecursion/Program.cs
+++ introToRecursion/Program.cs
@@ -6,15 +6,14 @@ namespace introToRecursion
     {
         static void Main(string[] args)
         {
-            int numbers = 0;
-            Console.WriteLine("Even Numbers : ");
-            evenNums(numbers);
-            Console.WriteLine("Done!");
-
+            int number = 0;
+            Console.WriteLine("Even Numbers: ");
+            evenNums(number);
+            Console.WriteLine("Done! ");
         }
         public static void evenNums(int i)
         {
-            if (i < 1000)
+            if (i <= 1000)
             {
                 Console.Write("{0} ", i);
                 i += 2;
