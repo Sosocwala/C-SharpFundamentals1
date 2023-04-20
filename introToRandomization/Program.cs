94403c6cc17ae59c6a15e20e2b4fb97056a7ff85
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-20 18:16:09 +0200

    C# fundamentals

--- introToRandomization/Program.cs
+++ introToRandomization/Program.cs
@@ -6,7 +6,31 @@ namespace introToRandomization
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            string fruit;
+            for (int i = 0; i < 10; i++)
+            {
+                int randomNums = random.Next(0, 4);
+                switch (randomNums)
+                {
+                    case 0:
+                        fruit = "a Orange";
+                        break;
+                    case 1:
+                        fruit = "an Apple";
+                        break;
+                    case 2:
+                        fruit = "a Mango";
+                        break;
+                    case 3:
+                        fruit = " an Grape ";
+                        break;
+                    default:
+                        fruit = "Nothing";
+                        return;
+                }
+                Console.WriteLine("You should have {0}", fruit);
+            }
         }
     }
 }
