494577987b033c28883ae2a45249f81cc10eb202
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-20 17:31:01 +0200

    C# fundamentals

---
+++ introToRandomization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace introToRandomization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
