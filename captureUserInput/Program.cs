d48eceb79b0d83bc0003607ce66626b420bc4a23
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-20 16:59:51 +0200

    C# fundamentals

--- captureUserInput/Program.cs
+++ captureUserInput/Program.cs
@@ -10,9 +10,9 @@ namespace captureUserInput
             string input = Console.ReadLine(); //read string from console
             Console.WriteLine("You entered {0}", input);
             int num;
-            if (int.TryParse(input, out num))
+            if (int.TryParse(input, out num)) //try to pass the string as integer
             {
-                Console.WriteLine("multyply by a 1000");
+                Console.Write("multyply by a 1000 :");
                 Console.WriteLine(num * 1000);
             }
 
