9e8804586d73f545c6bd8562b9e5164d34403230
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-21 22:05:47 +0200

    C# fundamentals

--- theQuickSortAlgorithm/Program.cs
+++ theQuickSortAlgorithm/Program.cs
@@ -30,6 +30,56 @@ namespace theQuickSortAlgorithm
                     x++;
                 }
             }
+            Console.WriteLine(Environment.NewLine + "Unsorted List : " + Environment.NewLine);
+            foreach (int i in Sorted)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "Pess ENTER");
+            Console.ReadLine();
+
+            quickSort(Sorted, 0, Sorted.Length - 1);
+            Console.WriteLine("Sorted List : " + Environment.NewLine);
+            foreach (int m in Sorted)
+            {
+                Console.Write("{0} ", m);
+            }
+            Console.WriteLine(Environment.NewLine + Environment.NewLine);
+
+        }
+        public static void quickSort(int[] sorted, int left, int right)
+        {
+            int r = left;
+            int l = right;
+            int pivot = sorted[(l + r) / 2];
+
+            while (l <= r)
+            {
+                while (sorted[l] < pivot)
+                {
+                    l++;
+                }
+                while (pivot < sorted[r])
+                {
+                    r--;
+                }
+                if (l <= r)
+                {
+                   int temp = sorted[l];
+                    sorted[l] = sorted[r];
+                    sorted[r] = temp;
+                    l++;
+                    r--;
+                }
+                if (left < r)
+                {
+                    quickSort(sorted, left, r);
+                }
+                if (l < right)
+                {
+                    quickSort(sorted, l, right);
+                }
+            }
         }
     }
 }
