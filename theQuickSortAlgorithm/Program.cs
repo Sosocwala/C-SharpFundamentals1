976bc1b9ca57e45fdbde20fc5111e67eb4b3a4b2
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-21 16:35:20 +0200

    C# fundamentals

--- theQuickSortAlgorithm/Program.cs
+++ theQuickSortAlgorithm/Program.cs
@@ -7,14 +7,23 @@ namespace theQuickSortAlgorithm
         static void Main(string[] args)
         {
             int[] Sorted = new int[100];
-            byte z = 0;
+            byte x = 0;
             Random random = new Random();
             bool addToArray = true;
 
-            while (z < 100)
+            while (x < 100)
             {
                 int randomNums = random.Next(1, 101);
                 addToArray = true;
+
+                for (int y = 0; y < x; y++)
+                {
+                    if (Sorted[y] == randomNums)
+                    {
+                        addToArray = false;
+                        break;
+                    }
+                }
             }
         }
     }
