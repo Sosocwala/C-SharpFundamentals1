b44fb5caa2abc6767f689b1d3d4f5b4642182aad
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-21 16:30:24 +0200

    C# fundamentals

---
+++ theQuickSortAlgorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace theQuickSortAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Sorted = new int[100];
+            byte z = 0;
+            Random random = new Random();
+            bool addToArray = true;
+
+            while (z < 100)
+            {
+
+            }
+        }
+    }
+}
