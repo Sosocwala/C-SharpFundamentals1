07d7031af5ea4b12df70fac996bf848554b4562d
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-23 14:46:17 +0200

    C# fundamentals

--- createFileWriteData/Program.cs
+++ createFileWriteData/Program.cs
@@ -9,6 +9,10 @@ namespace createFileWriteData
         static void Main(string[] args)
         {
             string textToAdd = "The big fat pig jumped in the mud";
-        }StreamReader outfile = new StreamReader()
+            StreamReader outfile = new StreamReader("../../NewFIle.text");
+        }
+        
+
+
     }
 }
