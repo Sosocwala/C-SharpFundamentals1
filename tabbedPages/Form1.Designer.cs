e27fe87311aa643d4bf1fa2b6ca882ed8f906b44
Sosocwala <sonwabilecwala6@gmail.com>
2023-04-24 15:07:52 +0200

    WindowsFormBasics

--- tabbedPages/Form1.Designer.cs
+++ tabbedPages/Form1.Designer.cs
@@ -64,9 +64,9 @@ namespace tabbedPages
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(554, 325);
+            this.tabPage2.Size = new System.Drawing.Size(554, 412);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "My Tab 2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
