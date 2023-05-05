1bd27486f781769e0576f38fd84fbb862f8d1c96
Sosocwala <sonwabilecwala6@gmail.com>
2023-05-05 21:42:14 +0200

    calculatorForm

--- calculaterApp/Form1.cs
+++ calculaterApp/Form1.cs
@@ -23,7 +23,7 @@ namespace calculaterApp
             bool totalFlag = false; 
 
             Button b = (Button)sender;
-            string inputTxt = 
+            string inputTxt = b.Text;
             
             
         }
