b14e84bef2523a50cfd65a2d24095e4ddf0c8fd9
Sosocwala <sonwabilecwala6@gmail.com>
2023-05-08 22:27:35 +0200

    C# fundamentals

--- theQuickSortAlgorithm/Program.cs
+++ theQuickSortAlgorithm/Program.cs
@@ -76,7 +76,7 @@ namespace theQuickSortAlgorithm
                 }
                 if (l < right)
                 {
-                    quickSort(sortedList, ) ;
+                    quickSort(sortedList, l,right) ;
                 }
             }
         }
