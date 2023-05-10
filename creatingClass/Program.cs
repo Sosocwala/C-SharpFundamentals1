2358fd7d22632927328844c7ea79c9a905faa745
Sosocwala <sonwabilecwala6@gmail.com>
2023-05-10 14:42:01 +0200

    ObjectOrientedProgramming

--- creatingClass/Program.cs
+++ creatingClass/Program.cs
@@ -12,7 +12,23 @@ namespace creatingClass
             private string kind;
             private double cost;
             private double amount;
+
+            public Fruit(string k, double c, double a)
+            {
+                kind = k;
+                cost = c;
+                amount = a;
+            }
+            public string getFruit()
+            {
+                return kind + "The cost is " + cost;
+            }
+            public double getCost()
+            {
+                return cost * amount;
+            }
         }
+
         
     }
 }
