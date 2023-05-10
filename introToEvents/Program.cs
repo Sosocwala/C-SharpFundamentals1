6e2e972dd31300b2d453aafff0bf454b36d2049e
Sosocwala <sonwabilecwala6@gmail.com>
2023-05-10 22:00:19 +0200

    ObjectOrientedProgramming

---
+++ introToEvents/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace introToEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit myFruit = new Fruit();
+            myFruit.NewEvent += new EventHandler(newKind);
+            myFruit.Kind = "Apple";
+            myFruit.Kind = "Banana";
+            myFruit.Kind = "Orange";
+            myFruit.Kind = "Cherry";
+        }
+        static void newKind(object newk, EventArgs e)
+        {
+            Fruit fruit = (Fruit)newk;
+            Console.WriteLine("The fruit has change to : {0}", fruit.Kind);
+        }
+        class Fruit
+        {
+            public event EventHandler NewEvent;
+            private string kind;
+            public string Kind
+            {
+                get { return kind; }
+                set
+                {
+                    kind = value;
+                    NewEvent(this, EventArgs.Empty);
+                }
+
+            }
+        }
+
+    }
+}
