d2771f92e28a5555fe3d5507d19ff4f3c262560d
Sosocwala <sonwabilecwala6@gmail.com>
2023-05-13 21:04:26 +0200

    ObjectOrientedProgramming

---
+++ introToInterfaces/ProgramBase.cs
@@ -0,0 +1,25 @@
+﻿namespace introToInterfaces
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            Apple grannySmith = new Apple { Amount = 12, Cost = 2.7 };
+            Apple novel = new Apple { Amount = 12, Cost = 2.6 };
+            int difference = grannySmith.CompareTo(novel); ;
+            string results = "";
+            if (difference == 0)
+            {
+                results = "the same price as";
+            }
+            else if (difference == 1)
+            {
+                results = "more expensive than the";
+            }
+            else
+            {
+                results = "less expensive than the";
+            }
+            Console.WriteLine("The Granny Smith is {0} Novel", results);
+    }
+}
